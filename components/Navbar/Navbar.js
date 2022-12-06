033226bbb75bcb1eafacfcea07a9003df11490a8
batimovil <german.figueroa@ort.edu.ar>
2022-12-06 17:59:32 -0300

    added mobile menu

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,15 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './navbar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faCartShopping, faBars } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { useRouter } from 'next/router';
+import grid from '../../styles/grid.module.css';
 
 const Navbar = ({ page }) => {
+  const [open, setOpen] = useState(false);
   const router = useRouter();
+
+  const mobileMenu = () => {
+    return (
+      <>
+        <FontAwesomeIcon
+          icon={faBars}
+          onClick={() => {
+            setOpen(!open);
+          }}
+          style={{ fontSize: 15, color: 'white', height: 15 }}
+        />
+
+        <div className={` ${open ? styles.open : styles.close} ${styles.mobileMenu} `}>
+          <ul>
+            <li>
+              <button
+                onClick={() => {
+                  router.push('/');
+                }}
+                className={page.page === 'products' ? styles.itemsNavDark : styles.itemsNav}
+              >
+                Inicio
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  router.push('/aboutus');
+                }}
+                className={page.page === 'products' ? styles.itemsNavDark : styles.itemsNav}
+              >
+                Nosotros
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  router.push('/products');
+                }}
+                className={page.page === 'products' ? styles.itemsNavDark : styles.itemsNav}
+              >
+                Productos
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  router.push('/contact');
+                }}
+                className={page.page === 'products' ? styles.itemsNavDark : styles.itemsNav}
+              >
+                Contacto
+              </button>
+            </li>
+          </ul>
+        </div>
+      </>
+    );
+  };
+
   return (
     <header className={page.page === 'products' ? styles.navProducts : styles.nav}>
-      <nav className={styles.menu}>
+      <nav className={`${styles.menu} ${grid.menu}`}>
         <ul className={styles.menuItems}>
           <li>
             <button
@@ -77,13 +139,13 @@ const Navbar = ({ page }) => {
             onClick={() => {
               router.push('/cart');
             }}
-            a
             className={page.page === 'products' ? styles.itemsNavDark : styles.itemsNav}
           >
             <FontAwesomeIcon icon={faCartShopping} style={{ fontSize: 15, color: 'white' }} />
           </button>
         </li>
       </ul>
+      {mobileMenu()}
     </header>
   );
 };
